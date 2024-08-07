a9c03b72e7bd0d71735c66eecccfda2748afae29
Thirumalesh Aaraveti <97395760+athiruma@users.noreply.github.com>
2024-08-07 16:45:34 +0530

    Added the artifacts, job markdown

--- simple_math.py
+++ simple_math.py
@@ -1,5 +1,15 @@
 # simple_math.py
 
+import sys
+
+
 def add(a, b):
     """Return the sum of a and b."""
     return a + b
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    a = int(args[1]) if len(args) > 1 else 1
+    b = int(args[2]) if len(args) > 2 else 3
+    print(add(a, b))
